9d04ce4fb28061dc8ccb8cb87fd4bc953780d7aa
Valentyn Saik <svbackend22@gmail.com>
2025-03-21 10:39:09 +0100

    small cleanup

--- backend/api.Tests/Integration/Kobo/Controllers/TmpBookBundleControllerTests.cs
+++ backend/api.Tests/Integration/Kobo/Controllers/TmpBookBundleControllerTests.cs
@@ -1,10 +1,7 @@
 using System.Net;
-using System.Net.Http.Json;
 using System.Text.Json;
 
-using api.Modules.Common.DTO;
 using api.Modules.Kobo.DTOs;
-using api.Modules.User.DTOs;
 
 using Microsoft.EntityFrameworkCore;
 
