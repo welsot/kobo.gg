994eb2b44409537d6e2ea47f4e2653b496b70d67
Valentyn Saik <svbackend22@gmail.com>
2025-03-23 20:53:20 +0100

    comment out the old way of uploading files via rust now we use js

--- mobile/src-tauri/src/lib.rs
+++ mobile/src-tauri/src/lib.rs
@@ -1,26 +1,26 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-async fn upload_file(url: String, data: Vec<u8>, content_type: String) -> Result<(), String> {
-    let client = reqwest::Client::new();
-    let result = client
-        .put(&url)
-        .header("Content-Type", content_type)
-        .body(data)
-        .send()
-        .await
-        .map_err(|e| e.to_string())?;
-
-    if result.status().is_success() {
-        Ok(())
-    } else {
-        Err(format!("Upload failed with status: {}", result.status()))
-    }
-}
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+//
+// #[tauri::command]
+// async fn upload_file(url: String, data: Vec<u8>, content_type: String) -> Result<(), String> {
+//     let client = reqwest::Client::new();
+//     let result = client
+//         .put(&url)
+//         .header("Content-Type", content_type)
+//         .body(data)
+//         .send()
+//         .await
+//         .map_err(|e| e.to_string())?;
+//
+//     if result.status().is_success() {
+//         Ok(())
+//     } else {
+//         Err(format!("Upload failed with status: {}", result.status()))
+//     }
+// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -37,7 +37,7 @@ pub fn run() {
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, upload_file])
+        //.invoke_handler(tauri::generate_handler![greet, upload_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
